6d88cc2ea603e84d687e7abb1e618847df967b85
Kiptoo  Moses <kiptoomoses156@gmail.com>
2025-06-19 14:25:45 +0300

    Create webclientConfig

---
+++ department-service/src/main/java/com/kiptoo/department_service/client/EmployeeClient.java
@@ -0,0 +1,4 @@
+package com.kiptoo.department_service.client;
+
+public interface EmployeeClient {
+}
---
+++ department-service/src/main/java/com/kiptoo/department_service/config/WebClientConfig.java
@@ -0,0 +1,4 @@
+package com.kiptoo.department_service.config;
+
+public class WebClientConfig {
+}
